a261a57755dbf0f939aa6d69761b466192c46695
MACBOOK <alexjace151@gmail.com>
2022-05-24 15:48:31 +0100

    Done with the forms

--- dashboardsuccess.php
+++ dashboardsuccess.php
@@ -88,7 +88,7 @@
                                 <p class="transdate">Transaction date</p>
                         </div>
                         <div class="col-md-6 col-sm-6 col-xs-6">
-                            <p class="money"><?php round($total_price, 2); ?> (NGN <?= number_format(round($paystackPayment->convertToNaira($total_price), 2)); ?>) ?></p>
+                            <p class="money"><?php round($total_price, 2); ?> (NGN <?= number_format(round($paystackPayment->convertToNaira($total_price), 2)); ?>)</p>
                               <p class="successname"><?= $user["firstname"]; ?></p>
                                 <p class="successdate"><?= date("d F Y", strtotime($payment["date"]));  ?></p>
                         </div>
