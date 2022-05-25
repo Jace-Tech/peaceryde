1ff38118e3b120ceee899ff1a33065d6b0da3ad8
MACBOOK <alexjace151@gmail.com>
2022-05-25 21:48:06 +0100

    done

--- Dashboard/handler/upload_handler.php
+++ Dashboard/handler/upload_handler.php
@@ -13,10 +13,11 @@ if(isset($_POST['upload'])) {
     $service = "NULL";
     $name = $_POST['name'];
 
+    print_r($_FILES);
+    die();
     $fileUpload = $uploads->uploadFile($_FILES["myFile"], "../upload/");
 
     print_r($fileUpload);
-    die();
     if($fileUpload['status'] == "success") {
         $uploaded = $uploads->uploadToDB($id, $fileUpload['file_name'], $name, $service);
 
