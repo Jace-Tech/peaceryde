051546b140b3c371fc74dac5f69aa833aba440bb
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-06-04 20:20:05 +0100

    Fixed the payment issue

--- payment.php
+++ payment.php
@@ -185,15 +185,25 @@ $PRICE = json_decode($_SESSION["PRICE"], true);
                         </p>
                     </div>
                 </div>
+                
+                <div class="row paymentdiv">
+                    <div class="col-lg-8 col-md-8 col-sm-8">
+                        <p class="paymentservices">VAT [7.5%]</p>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4">
+                        <p class="paymentprice"><?= round($PRICE['vat'], 2); ?></p>
+                    </div>
+                </div>
 
                 <div class="row paymentdiv">
                     <div class="col-lg-8 col-md-8 col-sm-8">
-                        <p class="paymentservices">VAT</p>
+                        <p class="paymentservices">GROSS [1.9914%]</p>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4">
-                        <p class="paymentprice"><?= round($PRICE['tax'], 2); ?></p>
+                        <p class="paymentprice"><?= round($PRICE['gross'], 2); ?></p>
                     </div>
                 </div>
+
                 <div class="reviewline"></div>
                 <div class="row paymentdiv">
                     <div class="col-lg-8 col-md-8 col-sm-8">
