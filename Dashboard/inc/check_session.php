1acbce745009e376ec5de4d91ddbc96c4465e94e
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-06-05 15:50:28 +0100

    done

--- Dashboard/inc/check_session.php
+++ Dashboard/inc/check_session.php
@@ -2,8 +2,8 @@
 
 session_start(); 
 
-// include("../db/config.php");
-include("../db/conf.php");
+include("../db/config.php");
+// include("../db/conf.php");
 
 include("../utils/country_fee.php");
 include("../functions/index.php");
