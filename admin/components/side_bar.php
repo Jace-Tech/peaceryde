4bfc0ba0f0f03858214e68cca012ac815b6aac59
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-06-08 21:17:02 +0100

    dashboard sidebar fixed

--- admin/components/side_bar.php
+++ admin/components/side_bar.php
@@ -23,7 +23,7 @@
         </div>
         <div class="space-y-8">
             <div>
-                <h3 class="text-xs uppercase text-gray-500 font-semibold pl-3">
+                <h3 class="text-xs uppercase text-white font-semibold pl-3">
                     <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span> 
                     <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">Pages</span>
                 </h3>
@@ -130,7 +130,7 @@
             </div>
             <?php if($LOGGED_USER['type'] === "HIGH"): ?>
                 <div>
-                    <h3 class="text-xs uppercase text-gray-500 font-semibold pl-3">
+                    <h3 class="text-xs uppercase text-white font-semibold pl-3">
                         <span class="hidden lg:block lg:sidebar-expanded:hidden 2xl:hidden text-center w-6" aria-hidden="true">•••</span> <span class="lg:hidden lg:sidebar-expanded:block 2xl:block">More</span></h3>
                     <ul class="mt-3">
                         <?php if(strtolower($active) === "faq"): ?>
