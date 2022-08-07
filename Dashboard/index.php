a9e5c9d57f207a8a1c91af62bddd9c7255eb6581
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-07 13:08:59 +0100

    updated

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -681,7 +681,7 @@ $isUnread = count($messages->get_user_unread_messages($USER_ID));
     <!-- <script src="./assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script> -->
     <script src="./assets/extra-libs/jvector/jquery-jvectormap-2.0.2.min.js"></script>
     <script src="./assets/extra-libs/jvector/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="./dist/js/pages/dashboards/dashboard1.min.js"></script>
+    <!-- <script src="./dist/js/pages/dashboards/dashboard1.min.js"></script> -->
 </body>
 
 </html>
