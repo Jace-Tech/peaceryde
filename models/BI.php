091bd9e4e153c7c0bbe85d41f1c3ff1cd5c14397
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-09 15:15:14 +0100

    Updated The BIForm handler

--- models/BI.php
+++ models/BI.php
@@ -11,19 +11,15 @@ class BI {
     {
         extract($bi_data);
 
-        $query = "INSERT INTO `bi_table`(`user_id`, `shares`, `email`, `company_name`, `coperate_address`, `company_email`, `website`, `employee_no`, `message`) 
-            VALUES (:userId, :shares, :email, :companyName, :coperateAddress, :companyEmail, :website, :employeeNo, :message)";
+        $query = "INSERT INTO `bi_table`(`user_id`, `shares`, `email`, `company_name`, `coperate_address`) 
+            VALUES (:userId, :shares, :email, :companyName, :coperateAddress)";
         $result = $this->connection->prepare($query);
         $result->execute([
             'userId' => $userId,
             'shares' => $shares ?? "",
             'email' => $email ?? "",
             'companyName' => $companyName ?? "",
-            'coperateAddress' => $coperateAddress ?? "",
-            'coperateEmail' => $coperateEmail ?? "",
-            'website' => $website ?? "",
-            'employeeNo' => $employeeNo ?? "",
-            'message' => $message ?? ""
+            'coperateAddress' => $coperateAddress ?? ""
         ]);
 
         return $result;
