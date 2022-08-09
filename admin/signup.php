096fe9327bdfe2c14df924fe6d4fd85440904e83
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-09 18:22:05 +0100

    Updated the admin logo

--- admin/signup.php
+++ admin/signup.php
@@ -36,7 +36,8 @@
 						<div class="flex-1">
 							<div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
 								<a class="block" href="index.php">
-									<svg width="32" height="32" viewBox="0 0 32 32">
+									<img src="../assets/peace.jpeg" class="w-[32px] h-[32px] rounded-full" alt="logo">
+									<!-- <svg width="32" height="32" viewBox="0 0 32 32">
 										<defs>
 											<linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
 												<stop stop-color="#A5B4FC" stop-opacity="0" offset="0%" />
@@ -51,7 +52,7 @@
 										<path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
 										<path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
 										<path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-									</svg>
+									</svg> -->
 								</a>
 							</div>
 						</div>
