7488c14b90ea54e3019703204efee49542a54a58
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-09 18:33:35 +0100

    Updated the admin logo in sidebar

--- admin/components/side_bar.php
+++ admin/components/side_bar.php
@@ -1,3 +1,10 @@
+<style>
+    .logo {
+        width: 55px;
+        height: 55px;
+        object-fit: contain;
+    }
+</style>
 <?php if (isset($active)) : ?>
     <div id="sidebar" class="flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-gray-800 p-4 transition-all duration-200 ease-in-out" :class="sidebarOpen ? 'translate-x-0' : '-translate-x-64'" @click.outside="sidebarOpen = false" @keydown.escape.window="sidebarOpen = false" x-cloak="lg" style="overflow-x: hidden;">
         <div class="flex justify-between mb-10 pr-3 sm:px-2">
@@ -5,7 +12,8 @@
                     <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
                 </svg></button>
             <a class="block" href="">
-                <svg width="32" height="32" viewBox="0 0 32 32">
+                <img src="../assets/peace.jpeg" class="logo" alt="logo">
+                <!-- <svg width="32" height="32" viewBox="0 0 32 32">
                     <defs>
                         <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
                             <stop stop-color="#A5B4FC" stop-opacity="0" offset="0%" />
@@ -20,7 +28,7 @@
                     <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
                     <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
                     <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-                </svg>
+                </svg> -->
             </a>
         </div>
         <div class="space-y-8">
