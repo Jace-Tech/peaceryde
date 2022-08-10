362a53615a13b014c5f2e36976ee95abe375ab4c
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-10 20:09:37 +0100

    checking

--- admin/components/side_bar.php
+++ admin/components/side_bar.php
@@ -5,6 +5,9 @@
         object-fit: contain;
     }
 </style>
+
+<?php print_r($LOGGED_ADMIN); ?>
+
 <?php if (isset($active)) : ?>
     <div id="sidebar" class="flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-gray-800 p-4 transition-all duration-200 ease-in-out" :class="sidebarOpen ? 'translate-x-0' : '-translate-x-64'" @click.outside="sidebarOpen = false" @keydown.escape.window="sidebarOpen = false" x-cloak="lg" style="overflow-x: hidden;">
         <div class="flex justify-between mb-10 pr-3 sm:px-2">
@@ -13,22 +16,6 @@
                 </svg></button>
             <a class="block" href="">
                 <img src="../assets/peace.jpeg" class="logo" alt="logo">
-                <!-- <svg width="32" height="32" viewBox="0 0 32 32">
-                    <defs>
-                        <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
-                            <stop stop-color="#A5B4FC" stop-opacity="0" offset="0%" />
-                            <stop stop-color="#A5B4FC" offset="100%" />
-                        </linearGradient>
-                        <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
-                            <stop stop-color="#38BDF8" stop-opacity="0" offset="0%" />
-                            <stop stop-color="#38BDF8" offset="100%" />
-                        </linearGradient>
-                    </defs>
-                    <rect fill="#6366F1" width="32" height="32" rx="16" />
-                    <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
-                    <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
-                    <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-                </svg> -->
             </a>
         </div>
         <div class="space-y-8">
@@ -242,82 +229,20 @@
                                 </a>
                             </li>
                         <?php endif; ?>
-
-                        <!-- <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0" x-data="{ open: false }">
-                            <a class="sidebar-expander-link block text-gray-50 hover:text-white transition duration-150" :class="open && 'hover:text-gray-100'" href="#0" @click.prevent="sidebarExpanded ? open = !open : sidebarExpanded = true">
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-center">
-                                        <svg class="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                                            <path class="fill-current text-gray-600" d="M8.07 16H10V8H8.07a8 8 0 110 8z" />
-                                            <path class="fill-current text-gray-400" d="M15 12L8 6v5H0v2h8v5z" /></svg> 
-                                            <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Authentication</span>
-                                        </div>
-                                    <div class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200"><svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400" :class="{ 'transform rotate-180': open }" viewBox="0 0 12 12">
-                                            <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" /></svg></div>
-                                </div>
-                            </a>
-                            <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                                <ul class="pl-9 mt-1" :class="{ 'hidden': !open }" x-cloak>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="signin.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Sign In</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="signup.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Sign up</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="reset-password.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Reset Password</span></a></li>
-                                </ul>
-                            </div>
-                        </li> -->
-                        <!-- <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0" x-data="{ open: false }"><a class="sidebar-expander-link block text-gray-50 hover:text-white transition duration-150" :class="open && 'hover:text-gray-100'" href="#0" @click.prevent="sidebarExpanded ? open = !open : sidebarExpanded = true">
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-center"><svg class="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                                            <path class="fill-current text-gray-600" d="M19 5h1v14h-2V7.414L5.707 19.707 5 19H4V5h2v11.586L18.293 4.293 19 5Z" />
-                                            <path class="fill-current text-gray-400" d="M5 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8Zm14 0a4 4 0 1 1 0-8 4 4 0 0 1 0 8ZM5 23a4 4 0 1 1 0-8 4 4 0 0 1 0 8Zm14 0a4 4 0 1 1 0-8 4 4 0 0 1 0 8Z" /></svg> <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Onboarding</span></div>
-                                    <div class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200"><svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400" :class="{ 'transform rotate-180': open }" viewBox="0 0 12 12">
-                                            <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" /></svg></div>
-                                </div>
-                            </a>
-                            <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                                <ul class="pl-9 mt-1" :class="{ 'hidden': !open }" x-cloak>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="onboarding-01.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Step 1</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="onboarding-02.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Step 2</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="onboarding-03.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Step 3</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" href="onboarding-04.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Step 4</span></a></li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li class="px-3 py-2 rounded-sm mb-0.5 last:mb-0" :class="{ 'bg-gray-900' style="background-color: #a0bd1c;": page.startsWith('component-') }" x-data="{ open: false }" x-init="$nextTick(() => open = page.startsWith('component-'))"><a class="sidebar-expander-link block text-gray-50 hover:text-white transition duration-150" :class="open && 'hover:text-gray-100'" href="#0" @click.prevent="sidebarExpanded ? open = !open : sidebarExpanded = true">
-                                <div class="flex items-center justify-between">
-                                    <div class="flex items-center"><svg class="shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                                            <circle class="fill-current text-gray-600" :class="page.startsWith('component-') && 'text-indigo-500'" cx="16" cy="8" r="8" />
-                                            <circle class="fill-current text-gray-400" :class="page.startsWith('component-') && 'text-indigo-300'" cx="8" cy="16" r="8" /></svg> <span class="text-sm font-medium ml-3 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Components</span></div>
-                                    <div class="flex shrink-0 ml-2 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200"><svg class="w-3 h-3 shrink-0 ml-1 fill-current text-gray-400" :class="{ 'transform rotate-180': open }" viewBox="0 0 12 12">
-                                            <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" /></svg></div>
-                                </div>
-                            </a>
-                            <div class="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                                <ul class="pl-9 mt-1" :class="{ 'hidden': !open }" x-cloak>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-button' && '!text-indigo-500'" href="component-button.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Button</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-form' && '!text-indigo-500'" href="component-form.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Input Form</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-dropdown' && '!text-indigo-500'" href="component-dropdown.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Dropdown</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-alert' && '!text-indigo-500'" href="component-alert.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Alert & Banner</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-modal' && '!text-indigo-500'" href="component-modal.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Modal</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-pagination' && '!text-indigo-500'" href="component-pagination.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Pagination</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-tabs' && '!text-indigo-500'" href="component-tabs.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Tabs</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-breadcrumb' && '!text-indigo-500'" href="component-breadcrumb.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Breadcrumb</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-badge' && '!text-indigo-500'" href="component-badge.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Badge</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-avatar' && '!text-indigo-500'" href="component-avatar.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Avatar</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-tooltip' && '!text-indigo-500'" href="component-tooltip.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Tooltip</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-accordion' && '!text-indigo-500'" href="component-accordion.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Accordion</span></a></li>
-                                    <li class="mb-1 last:mb-0"><a class="block text-gray-400 hover:text-gray-100 transition duration-150 truncate" :class="page === 'component-icons' && '!text-indigo-500'" href="component-icons.php"><span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Icons</span></a></li>
-                                </ul>
-                            </div>
-                        </li> -->
                     </ul>
                 </div>
             <?php endif; ?>
         </div>
         <div class="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
-            <div class="px-3 py-2"><button @click="sidebarExpanded = !sidebarExpanded"><span class="sr-only">Expand / collapse sidebar</span> <svg class="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
+            <div class="px-3 py-2">
+                <button @click="sidebarExpanded = !sidebarExpanded">
+                    <span class="sr-only">Expand / collapse sidebar</span> 
+                    <svg class="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
                         <path class="text-gray-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
                         <path class="text-gray-600" d="M3 23H1V1h2z" />
-                    </svg></button></div>
+                    </svg>
+                </button>
+            </div>  
         </div>
     </div>
 <?php endif; ?>
