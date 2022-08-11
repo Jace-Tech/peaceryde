d3ddeb2ef562262d9719770c41555dfa40e26171
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-11 20:32:19 +0100

    updated icons

--- admin/components/header.php
+++ admin/components/header.php
@@ -28,10 +28,7 @@ if(count($NOTIFICATIONS)) {
 				<div class="relative inline-flex" x-data="{ open: false }">
 					<button class="w-8 h-8 flex items-center justify-center bg-gray-100 hover:bg-gray-200 transition duration-150 rounded-full" :class="{ 'bg-gray-200': open }" aria-haspopup="true" @click.prevent="open = !open" :aria-expanded="open">
 						<span class="sr-only">Notifications</span>
-						<svg class="w-4 h-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-							<path class="fill-current text-gray-500" d="M6.5 0C2.91 0 0 2.462 0 5.5c0 1.075.37 2.074 1 2.922V12l2.699-1.542A7.454 7.454 0 006.5 11c3.59 0 6.5-2.462 6.5-5.5S10.09 0 6.5 0z" />
-							<path class="fill-current text-gray-400" d="M16 9.5c0-.987-.429-1.897-1.147-2.639C14.124 10.348 10.66 13 6.5 13c-.103 0-.202-.018-.305-.021C7.231 13.617 8.556 14 10 14c.449 0 .886-.04 1.307-.11L15 16v-4h-.012C15.627 11.285 16 10.425 16 9.5z" />
-						</svg>
+						<svg class="w-4 h-4" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path class="fill-current text-gray-500" d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z"></path></svg>
 						<?php if (count($UNREAD_NOTIFICATIONS)) : ?>
 							<div class="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 border-2 border-white rounded-full"></div>
 						<?php endif; ?>
@@ -68,7 +65,7 @@ if(count($NOTIFICATIONS)) {
 							<path class="fill-current text-gray-500" d="M6.5 0C2.91 0 0 2.462 0 5.5c0 1.075.37 2.074 1 2.922V12l2.699-1.542A7.454 7.454 0 006.5 11c3.59 0 6.5-2.462 6.5-5.5S10.09 0 6.5 0z" />
 							<path class="fill-current text-gray-400" d="M16 9.5c0-.987-.429-1.897-1.147-2.639C14.124 10.348 10.66 13 6.5 13c-.103 0-.202-.018-.305-.021C7.231 13.617 8.556 14 10 14c.449 0 .886-.04 1.307-.11L15 16v-4h-.012C15.627 11.285 16 10.425 16 9.5z" />
 						</svg>
-						<?php if (count($messages->get_user_unread_messages($LOGGED_ADMIN['admin_id'])) || count($UNREAD_NOTIFICATIONS)) : ?>
+						<?php if (count($messages->get_user_unread_messages($LOGGED_ADMIN['admin_id']))) : ?>
 							<div class="absolute top-0 right-0 w-2.5 h-2.5 bg-red-500 border-2 border-white rounded-full"></div>
 						<?php endif; ?>
 					</button>
