9176cb31bc212b8dfd232532906f7d406b98e4d2
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-13 14:09:17 +0100

    user-subadmin error

--- inc/footer.php
+++ inc/footer.php
@@ -253,4 +253,6 @@
     s0.parentNode.insertBefore(s1, s0);
   })();
 </script>
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<script src="./js/lang.js"></script> 
 <!--End of Tawk.to Script-->
