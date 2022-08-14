4a186e7817e36176d1aac69b8f667972b53229ee
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-14 21:23:37 +0100

    updated

--- functions/index.php
+++ functions/index.php
@@ -225,17 +225,8 @@ function getNotications ($connect, $id) {
 
             return array_filter($nots, function ($item) {
                 global $id;
-                print_r($id);
                 $arr = json_decode($item['admin'], true);
-
-                foreach ($arr as $adminId) {
-                    if($adminId == $id) {
-                        return true;
-                    }
-                    else {
-                        return false;
-                    }
-                }
+                return !in_array($id, $arr);
             });
         }
         else {
