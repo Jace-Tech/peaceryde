db8c02c9cae2615e0eb4abc2fa95ffac252c0c50
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-15 15:31:25 +0100

    users account update

--- Dashboard/handler/account_handler.php
+++ Dashboard/handler/account_handler.php
@@ -42,13 +42,75 @@ if(isset($_POST['update'])) {
         }
 
         try {
+            // Check if profile exits 
+            $query = "SELECT * FROM `uploads` WHERE `user_id` = :user AND `service_id` = :service";
+            $result = $connect->prepare($query);
+            $result->execute([
+                "user" => $USER_ID,
+                "service" => "PROFILE"
+            ]);
+
+            if($result->rowCount()) {
+                $prevData = $result->fetch();
+                // Delete file
+                $fileToDelete = json_decode($prevData['file'], true)[0];
+                if(unlink("../pic/$fileToDelete")){
+                    // Update Profile Info
+                    $query = "UPDATE `uploads` SET `file` = :filename WHERE `id` = :id";
+                    $result = $connect->prepare($query);
+                    $result->execute([
+                        'filename' => $filename,
+                        'id' => $prevData['id'],
+                    ]);
+
+                    if($result) {
+                        // Update user's info
+                        $query = "UPDATE `users` SET `title` = :title, `firstname` = :firstname, `middle_name`= :middle_name, `lastname` = :lastname, `email` = :email, `phone`= :phone WHERE `user_id` = :id";
+                        $result = $connect->prepare($query);
+                        $result->execute([
+                            "title" => $title,
+                            "firstname" => $firstname,
+                            "middle_name" => $middle_name,
+                            "lastname" => $lastname,
+                            "email" => $email,
+                            "phone" => $phone,
+                            "id" => $USER_ID
+                        ]);
+        
+                        if($result) {
+                            // Update users login table
+                            $hashedPassword = md5($password);
+                            $query = "UPDATE `user_login` SET `email` = :email, `password` = :password WHERE `user_id` = :user";
+                            $result = $connect->prepare($query);
+                            $result->execute([
+                                "email" => $email,
+                                "password" => $hashedPassword,
+                                "user" => $USER_ID
+                            ]);
+        
+                            if($result) {
+                                setUserAlert("Profile updated successfully", 'success');
+                                header("Location: ../account.php");
+                            }
+                            else {
+                                setUserAlert("Profile failed to update", "error");
+                                header("Location: ../upload.php");
+                            }
+        
+                        }
+                    }
+    
+                }
+
+                exit();
+            }
             // Update Profile Info
             $query = "INSERT INTO `uploads`(`user_id`, `name`, `file`, `service_id`, `status`) VALUES (:userid, :name, :filename, :service, :status)";
             $result = $connect->prepare($query);
             $result->execute([
                 'userid' => $USER_ID,
                 'name' => "PROFILE",
-                'filename' => json_encode($filename),
+                'filename' => $filename,
                 'service' => "PROFILE",
                 'status' => "Awaiting approval"
             ]);
