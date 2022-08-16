dc78a18649d5b83f00301eaeca3a14748c3b1d4a
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-16 16:41:50 +0100

    Fixed the language issue

--- inc/footer.php
+++ inc/footer.php
@@ -252,6 +252,36 @@
     s0.parentNode.insertBefore(s1, s0);
   })();
 </script>
+<script>
+  function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    includedLanguages: 'en,es,hi,pl,pt,zh-CN,zh-TW,ar,so,ru,hy,ko,fr,vi',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  }, 'google_translate_element');
+}
+
+function triggerHtmlEvent(element, eventName) {
+  var event;
+  if (document.createEvent) {
+    event = document.createEvent('HTMLEvents');
+    event.initEvent(eventName, true, true);
+    element.dispatchEvent(event);
+  } else {
+    event = document.createEventObject();
+    event.eventType = eventName;
+    element.fireEvent('on' + event.eventType, event);
+  }
+}
+
+jQuery('.lang-select').click(function() {
+  var theLang = jQuery(this).attr('data-lang');
+  jQuery('.goog-te-combo').val(theLang);
+
+  window.location = jQuery(this).attr('href');
+  location.reload();
+});
+
+</script> 
 <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-<script src="./js/lang.js"></script> 
 <!--End of Tawk.to Script-->
