84351473164cfe08e979e40055222b105a8a24b8
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-20 00:40:04 +0100

    added the translate in Dashboard

--- Dashboard/inc/sidebar.php
+++ Dashboard/inc/sidebar.php
@@ -2,7 +2,41 @@
     $messages = new Message($connect);
     $isUnread = $messages->get_user_unread_messages($USER_ID);
 ?>
-<?php include("../inc/langChange.php"); ?>
+<script>
+  function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    includedLanguages: 'en,bn,de,ur,el,cs,vi,ta,nl,it,ja,es,hi,pl,pt,zh-CN,zh-TW,ar,so,ru,hy,ko,fr,hu',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  }, 'google_translate_element');
+}
+
+function triggerHtmlEvent(element, eventName) {
+  var event;
+  if (document.createEvent) {
+    event = document.createEvent('HTMLEvents');
+    event.initEvent(eventName, true, true);
+    element.dispatchEvent(event);
+  } else {
+    event = document.createEventObject();
+    event.eventType = eventName;
+    element.fireEvent('on' + event.eventType, event);
+  }
+}
+
+jQuery('.lang-select').click(function() {
+  var theLang = jQuery(this).attr('data-lang');
+  jQuery('.goog-te-combo').val(theLang);
+
+  window.location = jQuery(this).attr('href')
+  localStorage.setItem('lang', theLang);
+  localStorage.setItem('langFunc', `${jQuery(this).attr('href')}`);
+  location.reload();
+});
+
+
+</script> 
+<script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <aside class="left-sidebar" data-sidebarbg="skin6" id="sidebar">
 
 
