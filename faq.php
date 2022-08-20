0d67abd3756db214f4a769df7686b6da35b0f73b
Lisa <lissyuloh8@gmail.com>
2022-08-19 23:52:01 -0700

    faq

--- faq.php
+++ faq.php
@@ -57,14 +57,13 @@
                     </h2>
                     <p class="everythingtext">
                         Everything you need to know
-                        about PeaceRyde Africa LLC
+                        about PeaceRyde Africa LLC and our services
                     </p>
                 </div>
                 <div>
                     <p class="everythingtext2">
-                        Everything you need to know
-                        about processing your
-                        Nigerian Visas and payment.
+                    Everything you need to know
+                        about PeaceRyde Africa LLC and our services
                     </p>
                     <?php if(count($FAQ->get_all_questions())): ?>
                         <?php $counter = 1;  ?>
