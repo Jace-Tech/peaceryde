4727c67bfd9365ee235f3f80abc68cf83dd20831
Lisa <lissyuloh8@gmail.com>
2022-08-21 08:22:15 -0700

    commit

--- BIForm.php
+++ BIForm.php
@@ -383,12 +383,13 @@
                                       <label class="form-label">Date of Birth</label>
                                       <div class="input-group mb-3 biwidth">
                                         <input type="text" readonly required name="dob" class="form-control firstname2" placeholder="dd/mm/yyyy" id="datepicker">
-                                        <p style="color: #C8730F;
+                                        
+                                      </div>
+                                      <p style="color: #C8730F;
     font-family: Ubuntu;
     font-size: 13px;
     font-style: normal;
     font-weight: 400;">You must be at least 18 years old to use this website.</p>
-                                      </div>
                                     </div>
                                   </div>
                                     <div class="col-md-4">
