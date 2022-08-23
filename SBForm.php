e7f70f903926ffbc7a1fb41bd0fde57258e14495
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-23 17:21:12 +0100

    js error

--- SBForm.php
+++ SBForm.php
@@ -365,17 +365,6 @@
           return
         }
       })
-
-      if (document.querySelector("#modal-yes").checked) {
-        onYes()
-      } else {
-        if (localStorage.getItem("USER_REG")) {
-          localStorage.removeItem("USER_REG");
-          <?php unset($_SESSION["REG_MODE"]); ?>
-        }
-        formElement.submit()
-
-      }
     })
   </script>
 
