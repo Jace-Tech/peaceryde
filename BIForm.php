fc4fd21403dcee685b0f011f68b9eb1a9bc45fa7
Lisa <lissyuloh8@gmail.com>
2022-08-26 20:50:52 -0700

    update

--- BIForm.php
+++ BIForm.php
@@ -485,8 +485,8 @@ if (isset($_SESSION['APPLY_FORM_DATA'])) {
                 <input type="text" required name="email" class="form-control" style="border: 1px solid #1161D9; width:100%;font-family:ubuntu;height: 44px; margin-top:27px;  color: #1161D9;" placeholder="Email" />
                 <input type="hidden" required name="redirect" value="../NBVForm.php">
                 <div class="input-group mb-3">
-                    <input type="password" id="id_password" required name="password" class="form-control" style="border: 1px solid #1161D9; width:100%;font-family:ubuntu;height: 44px; margin-top:27px; color: #1161D9;" placeholder="Password" />
-                    <span class="input-group-text fa fa-eye" id="togglePassword"></span>
+                <input type="password" id="id_password" required name="password" class="form-control" style="border: 1px solid #1161D9; width:90%;font-family:ubuntu;height: 44px; margin-top:27px; color: #1161D9;" placeholder="Password" />
+                    <span class="input-group-text fa fa-eye" style="height: 46px; margin-top: 26px;background: #1161D9; color: white;" id="togglePassword"></span>
                 </div> 
               </div>
             </div>
