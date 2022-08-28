820bfd37059aa64428d11d21f4f1617839ed24c1
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-08-28 07:55:16 +0100

    realtime

--- admin/realtime.js
+++ admin/realtime.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://peacerydeafrica.com/api"
 const getUnreadMessage = async (id) => {
     try {
         const request = await fetch(`${BASE_URL}/messanger.php?messenger=${id}`)
-        const response = await request.json()
+        const response = await request.text()
     
         return response
     } catch (err) {
