b285be60f8c78ef893c01177b4df4552d7e636e9
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-09-09 14:22:23 +0100

    fixed the contact-form

--- handlers/contact_handler.php
+++ handlers/contact_handler.php
@@ -2,6 +2,7 @@
 session_start();
 
 // require_once("../db/conf.php");
+
 require_once("../functions/index.php");
 require_once("../utils/country_fee.php");
 require_once("../utils/store.php");
@@ -12,13 +13,23 @@ require_once("../models/Message.php");
 if(isset($_REQUEST['contact'])) {
     $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
     $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
-    $from = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $userEmail = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
     $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+    
+    $from = "noreply@peacerydeafrica.com";
+    // $toEmail = "info@peacerydeafrica.com";
+    $toEmail = "jacealex151@gmail.com";
+    $admin = "PeaceRydeAfrica LLC";
+    $template = file_get_contents("../template/contact.html");
 
-    // $from = "info@peacerydeafrica.com";
-    $email = "jacealex151@gmail.com";
+    $template = str_replace("{{ admin }}", $admin, $template);
+    $template = str_replace("{{ name }}", $name, $template);
+    $template = str_replace("{{ message }}", $message, $template);
+    $template = str_replace("{{ email }}", $userEmail, $template);
+    $template = str_replace("{{ phone }}", $phone, $template);
 
-    sendMail("Contact Form Message", $message, $email, $from);
+
+    sendMail("Contact Form Message", $template, $from, $email, true);
     setUserAlert("Message sent", "success");  
 
     header("Location:" . $_SERVER['HTTP_REFERER']);
