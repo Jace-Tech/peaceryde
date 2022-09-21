d5e927aafa71e3ca5de10de9836e51f7eac21b3e
Lisa Uloh <lissyuloh8@gmail.com>
2022-09-21 14:06:54 -0700

    commit

--- review.php
+++ review.php
@@ -75,6 +75,53 @@ $users = new User($connect);
             width: 90%;
         } 
     }
+    .rating {
+            border: none;
+
+        }
+
+        .rating>input {
+            display: none;
+        }
+
+        .rating>label:before {
+            margin: 13px 5px 13px 13px;
+            font-size: 1.25em;
+            font-family: FontAwesome;
+            display: inline-block;
+            content: "\f005";
+        }
+
+        .rating>.half:before {
+            content: "\f089";
+            position: absolute;
+        }
+
+        .rating>label {
+
+            float: right;
+        }
+
+        /***** CSS Magic to Highlight Stars on Hover *****/
+
+        .rating>input:checked~label,
+        /* show gold star when clicked */
+        .rating:not(:checked)>label:hover,
+        /* hover current star */
+        .rating:not(:checked)>label:hover~label {
+            color: #FFD700;
+        }
+
+        /* hover previous stars in list */
+
+        .rating>input:checked+label:hover,
+        /* hover current star when changing rating */
+        .rating>input:checked~label:hover,
+        .rating>label:hover~input:checked~label,
+        /* lighten current selection */
+        .rating>input:checked~label:hover~label {
+            color: #FFED85;
+        }
     </style>
 </head>
 
@@ -86,7 +133,7 @@ $users = new User($connect);
             <section class=" new-section2__group layout2 reviewheight" style="background-color: #f8f6f6;">
                 <div class="new-section1__cover-block layout signinpage">
                     <!-- <h2 class="reviewh2">Review </h2> -->
-                    <h1 class="whoweare" style="margin-top:150px">
+                    <h1 class="whoweare" style="margin-top:100px; margin-left:0px; text-align:center">
                     Reviews
                     </h1>
 
@@ -127,6 +174,22 @@ $users = new User($connect);
                                                                 </span> 
                                                                 <?= $REVIEW_USER['country'] ?? ""; ?>
                                                             </p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                    <div class="moverating">
+                                                        <fieldset class="rating">
+                                                            <input type="radio" id="star5" name="rating" value="5" /><label class="full" for="star5" title="Awesome - 5 stars" style="margin-right: 110px;"></label>
+                                                            <input type="radio" id="star4half" name="rating" value="4.5" /><label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+                                                            <input type="radio" id="star4" name="rating" value="4" /><label class="full" for="star4" title="Pretty good - 4 stars"></label>
+                                                            <input type="radio" id="star3half" name="rating" value="3.5" /><label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+                                                            <input type="radio" id="star3" name="rating" value="3" /><label class="full" for="star3" title="Meh - 3 stars"></label>
+                                                            <input type="radio" id="star2half" name="rating" value="2.5" /><label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+                                                            <input type="radio" id="star2" name="rating" value="2" /><label class="full" for="star2" title="Kinda bad - 2 stars"></label>
+                                                            <input type="radio" id="star1half" name="rating" value="1.5" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+                                                            <input type="radio" id="star1" name="rating" value="1" /><label class="full" for="star1" title="Sucks big time - 1 star"></label>
+                                                            <input type="radio" id="starhalf" name="rating" value="0.5" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                                                        </fieldset>
                                                         </div>
                                                     </div>
                                                 </div>
