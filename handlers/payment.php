14243bfe8e0298bd116db0e4ab18e692c3d2d10a
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-10-08 17:25:17 +0100

    updated

--- handlers/payment.php
+++ handlers/payment.php
@@ -132,7 +132,7 @@ if(isset($_POST['pay'])){
                 'mode' => 'payment',
             ]);
 
-            echo $data['url'];
+            header("Location: {$data['url']}");
             break;
         default:
             break;
