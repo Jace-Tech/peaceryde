da778a948330fa5f72331b05547c88e2f3bb0285
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-10-26 00:35:36 +0100

    done

--- index.php
+++ index.php
@@ -90,6 +90,7 @@ $uploads = new Upload($connect);
   <style style="margin: 0; width: 0; height: 0;">
     #sendx-underlay-2oZvLk7I60tyBAcfn7dJXP {
       background-color: rgba(255, 255, 255, 0.3) !important;
+      opacity: 1;
     }
     #powered-by-sendx-content-2oZvLk7I60tyBAcfn7dJXP {
       display: none !important;
