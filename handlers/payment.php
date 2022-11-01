fc9cf4bb10575c2b65d10fa16381ef710faaedda
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-11-01 13:57:29 +0100

    switched to live mode

--- handlers/payment.php
+++ handlers/payment.php
@@ -28,8 +28,8 @@ $paystackPayment = new PaystackPayment("sk_live_67639b010ff3b1cb137d79632813db9a
 $id = $_SESSION['REG_NO'];
 $user = $users->get_user($id);
 
-// $stripe = new \Stripe\StripeClient('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');
-$stripe = new \Stripe\StripeClient('sk_test_51KmPCVFc4Ym6vBghFwZmRYh70pdYEkQI86MX4avEP5dm19jV0VrdoQGNJwnYUTMdWrj0Wjh0H1rMXuY5O5nE4EE300NxGc4po4');
+$stripe = new \Stripe\StripeClient('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');
+// $stripe = new \Stripe\StripeClient('sk_test_51KmPCVFc4Ym6vBghFwZmRYh70pdYEkQI86MX4avEP5dm19jV0VrdoQGNJwnYUTMdWrj0Wjh0H1rMXuY5O5nE4EE300NxGc4po4');
 
 if (isset($_POST['pay'])) {
     $POST = filter_var_array($_POST, FILTER_SANITIZE_STRING);
